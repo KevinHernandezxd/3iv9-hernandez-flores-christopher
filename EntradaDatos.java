f6074d4a9d96b365608de89a29f21f6d9b1e230d
kevonito hernandez <kevonito@MacBook-Air-de-kevonito.local>
2020-10-15 16:51:21 -0500

    Programa de entrada de datos

---
+++ EntradaDatos.java
@@ -0,0 +1,10 @@
+class EntradaDatos{
+
+    public static void main(String[] args){
+        String nombre;
+        System.out.println("Dime tu nombre plox");
+
+        nombre = System.console().readLine();
+        System.out.println("Bienvenido "+ nombre + " a programacion");
+    }
+}
---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+class HolaMundo{
+
+    public static void main(String[] args){
+        String nombre = "algo equisde";
+
+        System.out.println("El nombre es: " + nombre);
+    }
+}
